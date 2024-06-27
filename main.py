fcea9f21d71267a15ac303fe07cf689689c54e3e
BoldyrevaMarya <matazzz@mail.ru>
2024-06-27 17:26:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Hello world!")
+a1= "произвольная строка"
+print(len(a1))
+first = 5
+second = 6
+print((first+second),(first-second))
+summa = 11
+diff = -1
+print(summa,diff)
+first = 5
+second = 10
+third = 15
+print((first+second+third)//3)
+mean = 10
+print(mean)
+first_string = ("Вторник")
+second_string = ("Понедельник")
+print((second_string)+(", ")+(first_string))
+a=2
+b=5
+c=3
+f=((a*b)+(a*c))
+print((f**3)//2)
